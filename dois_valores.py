6cbbe0e5d5e7d3cad6eef8aad49049c1b33c6636
C4rv4lh0 <rodrigo.carvalho.1998@gmail.com>
2024-08-26 13:41:43 -0400

    Python 26/08

---
+++ dois_valores.py
@@ -0,0 +1,7 @@
+valor_1 = input("Informe o primeiro valor: ")
+valor_2 = input("Informe o segundo valor: ")
+
+if valor_1 >= valor_2:
+    print(f"O primeiro valor: '{valor_1}', é maior ou igual que o segundo '{valor_2}'")
+else:
+    print(f"O segundo valor: '{valor_2}', é maior que o primeiro valor: '{valor_1}'")
---
+++ imc.py
@@ -0,0 +1,8 @@
+nome = "Rodrigo Moura"
+altura = 1.76
+peso = 98
+imc = peso / altura ** 2
+
+print(f"{nome} tem {altura:.2f} de altura")
+print(f"pesa {peso} quilos e seu imc é")
+print(f"{imc:.2f}")
